713c2bcf54e9951c71a65b33b4abbca6454f1561
Elena Sorokina <chikenjam11@gmail.com>
2023-10-29 01:34:15 +0300

    Initial commit
    additional task

--- src/main/java/TriangleArea.java
+++ src/main/java/TriangleArea.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class TriangleArea {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-                System.out.println("Сторона a: ");
-                int a = scanner.nextInt();
+        System.out.println("Сторона a: ");
+        int a = scanner.nextInt();
 
-                System.out.println("Сторона b: ");
-                int b = scanner.nextInt();
+        System.out.println("Сторона b: ");
+        int b = scanner.nextInt();
 
-                int area = (a*b )/2;
-                System.out.println(" Плошадь треугольника равна " + area);
+        int area = (a * b) / 2;
+        System.out.println(" Плошадь треугольника равна " + area);
 
     }
 }
