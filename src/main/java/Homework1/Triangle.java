d6936e4bb109985852a16c9b19b83e7bd2a9a911
Elena Sorokina <chikenjam11@gmail.com>
2023-10-31 13:27:45 +0300

    Initial commit
    additional task

--- src/main/java/Homework1/Triangle.java
+++ src/main/java/Homework1/Triangle.java
@@ -5,6 +5,7 @@ public class Triangle {
     public static void main(String[] args) {
         System.out.printf("Площадь %.2f", geroneSquare(2, 2, 2));
     }
+
     public static double geroneSquare(double a, double b, double c) {
         double p = (a + b + c) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
